36427d7735539c1163035dc47283026368aa9e3d
Gumerov Artur <2diver7@gmail.com>
2023-09-03 23:53:40 +0300

    Перенос файлов из форка Erblack в отдельный репозиторий. Обновление npm и node.

---
+++ .eleventy.js
@@ -0,0 +1,47 @@
+const yaml = require('yaml');
+const Image = require('@11ty/eleventy-img');
+
+async function imageShortcode(src, alt, className) {
+    const {
+        webp: [data],
+    } = await Image(src, {
+        outputDir: 'public/wishlist/img',
+        widths: [640],
+        formats: ['webp'],
+    });
+
+    return `<img class="${className}" src="/wishlist${data.url}" alt="${alt}" loading="lazy" decoding="async">`;
+}
+
+async function srcShortcode(src) {
+    const {
+        jpeg: [{ url }],
+    } = await Image(src, {
+        outputDir: 'public/wishlist/img',
+        widths: [512],
+        formats: ['jpeg'],
+    });
+
+    return url;
+}
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setBrowserSyncConfig({
+        files: '/public/static/**/*.css',
+    });
+    eleventyConfig.addDataExtension('yml', contents => yaml.parse(contents));
+    eleventyConfig.addLiquidFilter('isUrl', value => String(value).indexOf('http') === 0);
+    eleventyConfig.addLiquidFilter('host', value => new URL(value).host);
+    eleventyConfig.addLiquidFilter('filterWanted', (items, type) =>
+        items.filter(({ realized }) => (type !== 'wanted' ? realized : !realized))
+    );
+    eleventyConfig.addLiquidShortcode('image', imageShortcode);
+    eleventyConfig.addLiquidShortcode('src', srcShortcode);
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'public',
+        },
+    };
+};
